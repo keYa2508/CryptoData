64fa1dd3c2e7a7a4faa96a9c6ce15186b6ea4070
keYa2508 <karthi25082000@gmail.com>
2023-12-15 17:21:35 +0530

    Final change

--- App.tsx
+++ App.tsx
@@ -1,9 +1,19 @@
-import {StyleSheet, Text, View} from 'react-native';
+/* eslint-disable react-native/no-inline-styles */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import {ActivityIndicator, FlatList, Text, View} from 'react-native';
 import React, {useEffect, useState} from 'react';
 import io, {Socket} from 'socket.io-client';
 
+interface OrderBookData {
+  bids: {price: number; qty: number}[];
+  asks: {price: number; qty: number}[];
+}
+
 const App = () => {
-  const [orderBookData, setOrderBookData] = useState<any>([]);
+  const [orderBookData, setOrderBookData] = useState<OrderBookData>({
+    bids: [],
+    asks: [],
+  });
   const [isSocketConnected, setIsSocketConnected] = useState<boolean>(false);
 
   useEffect(() => {
@@ -17,14 +27,16 @@ const App = () => {
 
     socket.on('connect', () => {
       setIsSocketConnected(true);
+      console.log('isSocketConnected', isSocketConnected);
     });
 
     socket.on('disconnect', () => {
       setIsSocketConnected(false);
     });
 
-    socket.on('event', data => {
-      setOrderBookData(data);
+    socket.on('events', data => {
+      setOrderBookData(data.data);
+      console.log(orderBookData);
     });
 
     return () => {
@@ -32,12 +44,72 @@ const App = () => {
     };
   }, []);
   return (
-    <View>
-      <Text>App</Text>
+    <View style={{flex: 1, padding: 20, backgroundColor: 'white'}}>
+      <Text style={{color: '#2f3030', fontSize: 40, fontWeight: '900'}}>
+        Order Book
+      </Text>
+      {isSocketConnected ? (
+        <View>
+          <View>
+            <Text style={{color: 'red', fontSize: 30, fontWeight: '900'}}>
+              Asks
+            </Text>
+            <FlatList
+              showsVerticalScrollIndicator={false}
+              data={orderBookData.asks}
+              renderItem={({item}) => (
+                <View
+                  style={{
+                    flexDirection: 'row',
+                    justifyContent: 'space-between',
+                  }}>
+                  <View style={{width: 200}}>
+                    <Text style={{color: 'red'}}>
+                      Quantity: {item.qty.toFixed(5)}
+                    </Text>
+                  </View>
+                  <View style={{width: 100}}>
+                    <Text style={{color: 'red'}}>Price: {item.price}</Text>
+                  </View>
+                </View>
+              )}
+              ListEmptyComponent={<ActivityIndicator color={'red'} size={50} />}
+            />
+          </View>
+          <View>
+            <Text style={{color: '#34ed56', fontSize: 30, fontWeight: '900'}}>
+              Bids
+            </Text>
+            <FlatList
+              showsVerticalScrollIndicator={false}
+              data={orderBookData.bids}
+              renderItem={({item}) => (
+                <View
+                  style={{
+                    flexDirection: 'row',
+                    justifyContent: 'space-between',
+                  }}>
+                  <View style={{width: 200}}>
+                    <Text style={{color: '#34ed56'}}>
+                      Quantity: {item.qty.toFixed(5)}
+                    </Text>
+                  </View>
+                  <View style={{width: 100}}>
+                    <Text style={{color: '#34ed56'}}>Price: {item.price}</Text>
+                  </View>
+                </View>
+              )}
+              ListEmptyComponent={
+                <ActivityIndicator color={'#34ed56'} size={50} />
+              }
+            />
+          </View>
+        </View>
+      ) : (
+        <Text style={{color: '#2f3030'}}>Connecting to WebSocket...</Text>
+      )}
     </View>
   );
 };
 
 export default App;
-
-const styles = StyleSheet.create({});
